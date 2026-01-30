45c93154b7b4d3ae4f6132e7396a8f7565f76ef1
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-01-30 14:58:45 +0100

    Update example_steps.rb

--- features/step_definitions/example_steps.rb
+++ features/step_definitions/example_steps.rb
@@ -3,9 +3,9 @@ When /I navigate to a webpage/ do
 end
 
 When /I interact with the webpage/ do
-  click_on("About Ruby", match: :first)
+  click_on("Install", match: :first)
 end
 
 When /I check the content of the webpage/ do
-  page.has_content?("Ruby is a language of careful balance.")
+  page.has_content?("you have plenty of options to install and manage Ruby")
 end
