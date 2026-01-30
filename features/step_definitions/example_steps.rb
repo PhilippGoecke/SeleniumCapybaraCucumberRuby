10298d029e5f02fed6cdce65c8f4517e5f4d2371
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-01-30 15:05:28 +0100

    Update step definitions for webpage interactions

--- features/step_definitions/example_steps.rb
+++ features/step_definitions/example_steps.rb
@@ -3,9 +3,9 @@ When /I navigate to a webpage/ do
 end
 
 When /I interact with the webpage/ do
-  click_on("Install", match: :first)
+  click_on("Download", match: :first)
 end
 
 When /I check the content of the webpage/ do
-  page.has_content?("you have plenty of options to install and manage Ruby")
+  page.has_content?("Here you can get the latest Ruby distributions in your favorite flavor.")
 end
